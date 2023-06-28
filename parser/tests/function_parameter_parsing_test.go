d687bf044a8ff1ed9adf13f9b65e31eed1f031a5
M <github@emmanull.com>
2023-06-28 21:27:19 +0200

    Function literal parsing

---
+++ parser/tests/function_parameter_parsing_test.go
@@ -0,0 +1,35 @@
+package parser_tests
+
+import (
+	"testing"
+
+	"github.com/0xM-D/interpreter/ast"
+	"github.com/0xM-D/interpreter/lexer"
+	"github.com/0xM-D/interpreter/parser"
+)
+
+func TestFunctionParameterParsing(t *testing.T) {
+	tests := []struct {
+		input          string
+		expectedParams []string
+	}{
+		{input: "fn() {};", expectedParams: []string{}},
+		{input: "fn(x) {};", expectedParams: []string{"x"}},
+		{input: "fn(x, y, z) {};", expectedParams: []string{"x", "y", "z"}},
+	}
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
+		stmt := program.Statements[0].(*ast.ExpressionStatement)
+		function := stmt.Expression.(*ast.FunctionLiteral)
+		if len(function.Parameters) != len(tt.expectedParams) {
+			t.Errorf("length parameters wrong. want %d, got=%d\n",
+				len(tt.expectedParams), len(function.Parameters))
+		}
+		for i, ident := range tt.expectedParams {
+			testLiteralExpression(t, function.Parameters[i], ident)
+		}
+	}
+}
